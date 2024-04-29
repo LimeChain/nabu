2624320eee2f794b3355c82d70cc79935905b9f6
David Petrov <david.petrov@limechain.tech>
2024-04-29 15:28:02 +0300

    fix!: merge mishaps

--- src/test/java/org/peergos/BitswapTest.java
+++ src/test/java/org/peergos/BitswapTest.java
@@ -49,11 +49,11 @@ public class BitswapTest {
     @Test
     public void getTenBlocks() {
         HostBuilder builder1 = HostBuilder.create(TestPorts.getPort(),
-                new RamProviderStore(1000), new RamRecordStore(), new RamBlockstore(), (c, p, a) -> CompletableFuture.completedFuture(true));
+                new RamProviderStore(1000), new RamRecordStore(), new RamBlockstore(), (c, p, a) -> CompletableFuture.completedFuture(true), false);
         Host node1 = builder1.build();
         RamBlockstore blockstore2 = new RamBlockstore();
         HostBuilder builder2 = HostBuilder.create(TestPorts.getPort(),
-                new RamProviderStore(1000), new RamRecordStore(), blockstore2, (c, p, a) -> CompletableFuture.completedFuture(true));
+                new RamProviderStore(1000), new RamRecordStore(), blockstore2, (c, p, a) -> CompletableFuture.completedFuture(true), false);
         Host node2 = builder2.build();
         node1.start().join();
         node2.start().join();
